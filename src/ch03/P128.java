893d8305502ecd5396ef9f0629e3e455cb12b769
kusuda-5150 <kusuda925@gmail.com>
2026-04-20 23:36:09 +0900

    //

---
+++ src/ch03/P128.java
@@ -0,0 +1,9 @@
+package ch03;
+
+public class P128 {
+    public static void main (String[] args) {
+        for(int i = 0; i < 3; i++) {
+            System.out.print("現在" + (i + 1) + "周目→");
+        }
+    }
+}
