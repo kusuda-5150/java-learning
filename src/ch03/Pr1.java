33f1c1c0a72af5d9be1774bd5bb445227051769a
kusuda-5150 <kusuda925@gmail.com>
2026-04-21 23:55:14 +0900

    //

---
+++ src/ch03/Pr1.java
@@ -0,0 +1,21 @@
+package ch03;
+
+public class Pr1 {
+    public static void mail(String[] args) {
+        //3-1
+
+        /* weight == 60;
+        (age1 + age2) * 2 > 60;
+        age % 2 == 1;
+        name.equals("港");
+         */
+
+        //3-2
+
+        //C, E
+
+        //3-3
+
+
+    }
+}
