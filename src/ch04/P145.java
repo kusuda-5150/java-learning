58b3419f942a189abe894a0e40cba035f47dfa51
kusuda-5150 <kusuda925@gmail.com>
2026-04-23 05:36:24 +0900

    //

---
+++ src/ch04/P145.java
@@ -0,0 +1,9 @@
+package ch04;
+
+public class P145 {
+    public static void main(String[] args) {
+        int[] scores = new int[5];
+        int num = scores.length;
+        System.out.println("要素の数：" + num);
+    }
+}
---
+++ src/ch04/P150.java
@@ -0,0 +1,12 @@
+
+package ch04;
+
+public class P150 {
+    public static void main(String[] args) {
+        int[] scores = {20,30,40,50,80};
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+
+    } 
+}
