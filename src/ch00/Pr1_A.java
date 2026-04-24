12705410ef687550db3304dca4bac9e2619dd544
kusuda-5150 <kusuda925@gmail.com>
2026-04-25 01:45:36 +0900

    //

---
+++ src/ch00/Pr1_A.java
@@ -0,0 +1,20 @@
+package ch00;
+
+public class Pr1_A {
+    public static void main(String[] args) {
+
+        String songName = "Blue Mirage";
+        String songDiff = "Another";
+        int notesNum = 1800;
+        int nowBp = 72;
+        double scoreRate = 83.45;
+        double pseudoGauge = 64.8;
+
+        System.out.println("曲名：" + songName);
+        System.out.println("難易度：" + songDiff);
+        System.out.println("ノーツ数：" + notesNum);
+        System.out.println("今回BP：" + nowBp);
+        System.out.println("スコア率：" + scoreRate);
+        System.out.println("疑似HARDゲージ：" + 64.8 + "%");
+    }
+}
